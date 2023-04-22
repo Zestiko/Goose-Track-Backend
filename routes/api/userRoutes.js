0fc76a0be41f7673613a905c8c8d0a75afd9d5a5
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2023-04-22 18:20:58 +0300

    updated controller

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const updateUserAndAvatar = require("../../controllers/users/updateUserandAvatar");
+
 
 const router = express.Router();
 
 router.get("/current", (req, res, next) => {});
 router.post("/logout", (req, res, next) => { });
 
-router.patch("/info", (req, res, next) => {});
+router.patch("/info", updateUserAndAvatar);
 
 module.exports = router;
