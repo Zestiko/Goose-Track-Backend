680f7c14d491b21fce46510b900a209e59c0d568
IvanKoshel <educationZak@gmail.com>
2023-04-22 16:45:16 +0300

    Create : - userInfoPatcValidator; - .prettierrc.js; - constants/index.js; - AVATAR_URL_REGEX.js; - PHONE_NIMBER_REGEX.js Corrected: - .eslintr.js

---
+++ constants/index.js
@@ -0,0 +1,13 @@
+const AVATAR_URL_REGEX = require('./AVATAR_URL_REGEX');
+const COLUMNS_TITLE = require('./COLUMNS_TITLE');
+const PASSWD_REGEX = require('./PASSWD_REGEX');
+const PHONE_NUMBER_REGEX = require('./PHONE_NUMBER_REGEX');
+const tasksPriority = require('./tasksPriority');
+
+module.exports = {
+  COLUMNS_TITLE,
+  PASSWD_REGEX,
+  tasksPriority,
+  AVATAR_URL_REGEX,
+  PHONE_NUMBER_REGEX,
+};
