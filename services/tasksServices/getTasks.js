f7c3101fae0faf5e26e1e2091d33a55efefd7449
Oleksandr P <goodwin009@gmail.com>
2023-04-25 21:04:25 +0300

    update getTasks to get tasks by currentMonth

--- services/tasksServices/getTasks.js
+++ services/tasksServices/getTasks.js
@@ -1,14 +1,29 @@
-const Task = require('../../models/tasksModel')
-const { AppError } = require('../../utils')
+const Task = require('../../models/tasksModel');
+const { AppError } = require('../../utils');
 
-const getTasks = async(owner)=> {
-    const query = {
-        owner,
-    }
-    const tasks = await Task.find(query)
-    if(!tasks) {
-        throw new AppError(400, "Error. Failed to get tasks.")
-    }
-    return tasks;
-}
+const getTasks = async (owner, date) => {
+  const currentDate = date || new Date();
+  const firstDayOfMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    1
+  );
+  const lastDayOfMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + 1,
+    0,
+    23,
+    59,
+    59
+  );
+
+  const tasks = await Task.find({
+    owner,
+    taskDate: { $gte: firstDayOfMonth, $lte: lastDayOfMonth },
+  });
+  if (!tasks) {
+    throw new AppError(400, 'Error. Failed to get tasks.');
+  }
+  return tasks;
+};
 module.exports = getTasks;
