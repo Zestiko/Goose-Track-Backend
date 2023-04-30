5e05ae5f5c7557de24a3065d891ed680ea2cf314
Alieksieiev Vladyslav <kat9.hora@gmail.com>
2023-04-30 22:27:30 +0300

    Update validateUserData.js

--- middleware/validateUser/validateUserData.js
+++ middleware/validateUser/validateUserData.js
@@ -2,8 +2,8 @@ const { catchAsync, AppError } = require('../../utils');
 const validateUserInfo = require('../../utils/Joi/validateUserInfo');
 
 const validateUserData = catchAsync(async (req, _, next) => {
-  if (Object.getOwnPropertyNames(req.body).length === 0)
-    return next(new AppError(400, 'Missing fields'));
+  // if (Object.getOwnPropertyNames(req.body).length === 0)
+  //   return next(new AppError(400, 'Missing fields'));
 
   const { error, value } = validateUserInfo(req.body);
 
