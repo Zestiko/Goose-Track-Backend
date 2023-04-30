5c0615639f53c895149b7d4575f866e609437ffe
Alieksieiev Vladyslav <kat9.hora@gmail.com>
2023-04-30 04:35:32 +0300

    fix bugs

--- controllers/users/patchUserInfo.js
+++ controllers/users/patchUserInfo.js
@@ -3,7 +3,7 @@ const editUserInfo = require('../../services/userServices/editUser');
 const { catchAsync } = require('../../utils');
 
 const patchUserInfo = catchAsync(async (req, res) => {
-  req.body.avatar = req.file.path;
+  req.body.avatar = req.file?.path;
 
   const newUserInfo = await editUserInfo(req.user.id, req.body);
 
--- utils/Joi/validateUserInfo.js
+++ utils/Joi/validateUserInfo.js
@@ -4,7 +4,7 @@ const { PHONE_NUMBER_REGEX, AVATAR_URL_REGEX } = require('../../constants');
 const validateUserInfo = data =>
   Joi.object({
     userName: Joi.string().min(3).max(50).optional(),
-    birthday: Joi.date().format('YYYY-MM-DD').optional(),
+    birthday: Joi.date().raw().optional(),
     email: Joi.string().email().optional(),
     phone: Joi.string().regex(PHONE_NUMBER_REGEX).optional(),
     telegram: Joi.string().min(3).optional(),
